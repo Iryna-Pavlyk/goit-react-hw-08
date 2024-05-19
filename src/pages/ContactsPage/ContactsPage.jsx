29668916f0f987469a742c64a56586980ffeedc7
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-05-19 11:57:03 +0300

    up

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,4 +1,6 @@
 import ContactForm from "../../components/ContactForm/ContactForm";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import ContactList from "../../components/ContactList/ContactList";
 import css from "./ContactsPage.module.css";
 
 const ContactsPage = () => {
@@ -6,6 +8,8 @@ const ContactsPage = () => {
     <>
       <h3 className={css}>My contacts</h3>
       <ContactForm />
+      <SearchBox />
+      <ContactList />
     </>
   );
 };
